1c2476449c33c17ea3046c3605a82eface8eccd4
Jeffrey <hernew@gmail.com>
2022-07-28 20:14:48 -0700

    added sample api call - GET requests work/ set up separate component folders

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Component } from "react";
 
-import logo from "./logo.svg";
+import CardList from "./components/card-list/card-list.component";
 import "./App.css";
 
 class App extends Component {
@@ -8,37 +8,61 @@ class App extends Component {
     super();
 
     this.state = {
-      name: { firstName: "Jeff", lastName: "Young" },
-      company: "ZTM",
+      monsters: [],
+      searchField: "",
     };
+    console.log("constructor");
   }
 
+  componentDidMount() {
+    console.log("component did mount");
+    fetch("https://jsonplaceholder.typicode.com/users").then((response) =>
+      response.json().then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      )
+    );
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
   render() {
+    console.log("render");
+
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at{" "}
-            {this.state.company}
-          </p>
-          <button
-            onClick={() => {
-              this.setState(
-                () => {
-                  return {
-                    name: { firstName: "Lo", lastName: "Elias" },
-                  };
-                },
-                () => {
-                  console.log(this.state);
-                }
-              );
-            }}
-          >
-            Change Name
-          </button>
-        </header>
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={onSearchChange}
+        />
+        {/* {filteredMonsters.map((monster) => {
+          return (
+            <div key={monster.name}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })} */}
+        <CardList />
       </div>
     );
   }
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class CardList extends Component {
+    render() {
+        return ( 
+        <div>I am the CardList Component </div>
+        )
+    }
+}
+
+export default CardList;
